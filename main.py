22d2ca52f15924f68f97f87402e534178b973f9e
Lucas Galan <97174691+LG-OCSB@users.noreply.github.com>
2022-02-01 14:00:36 +0000

    Final version of rst

--- main.py
+++ main.py
@@ -1,12 +1,59 @@
-# NAME OF AUTHOR:  
-# NAME OF THE PROGRAM:  
-# DATE OF CREATION:  
-# PURPOSE OF PROGRAM:  
+# NAME OF AUTHOR:  Lucas Galan
+# NAME OF THE PROGRAM: Amougs SUSSY QUIZ 
+# DATE OF CREATION:  febuary 1st 2022
+# PURPOSE OF PROGRAM:  to test user in critical thinking skills
 
-# VARIABLE DEFINITION
+from questions import quiz
 
-# INPUT
 
-# PROCESSING
+def check_ans(question, ans, attempts, score):
+    #Amogs checks for question answer and failure
+    
+    
+    if quiz[question]['answer'].lower() == ans.lower():
+        print(f"Correct Answer! \nYour score is {score + 1}!ඞඞඞ")
+        return True
+    else:
+        print(f"Wrong Answer :( \nYou have {attempts - 1} left cause ur garbageඞඞඞ! \nTry again idiotඞඞඞ")
+        return False
 
-# OUTPUT
+
+def print_dictionary():
+    for question_id, ques_answer in quiz.items():
+        for key in ques_answer:
+            print(key + ':', ques_answer[key])
+
+#the intro message
+def intro_message():
+    
+    
+    
+    print("Amogus quiz for non sussy people!")
+    print("You get 6 questions. You can type skip cause ur bad")
+    input("Press any key to start the sussy quiz ඞ ")
+    return True
+
+
+# Shows the messages and the scoring system
+intro = intro_message()
+while True:
+    score = 0
+    for question in quiz:
+        attempts = 3
+        while attempts > 0:
+            print(quiz[question]['question'])
+            answer = input("Enter Answer (To skip type skip. when you run out of attempts just skip to next question. If you run out of attempts ur badඞඞඞඞඞඞඞඞඞ) ")
+            if answer == "skip":
+                break
+            check = check_ans(question, answer, attempts, score)
+            if check:
+                score += 1
+                break
+            attempts -= 1
+
+    break
+#the end game
+print(f"Your final score is {score}!\n\n you sussy bakaඞ")
+print("Want to know the correct answers? Please see them below! ;)\n")
+print_dictionary()
+print("Thanks for playing you sussy baka.")
---
+++ questions.py
@@ -0,0 +1,26 @@
+quiz = {
+    1 : {
+        "question" : "Who is Imposter??????",
+        "answer" : "red"
+    },
+    2 : {
+        "question" : "Who is sus?",
+        "answer" : "red"
+    },
+    3 : {
+        "question" : "Did red vent?",
+        "answer" : "yes"
+    },
+    4 : {
+        "question" : "Is green sus?",
+        "answer" : "no"
+    },
+    5 : {
+        "question" : "Who is 2 imposter?????",
+        "answer" : "red"
+    },
+    6 : {
+        "question" : "Who is 3 imposter?",
+        "answer" : "red"
+    }
+}
